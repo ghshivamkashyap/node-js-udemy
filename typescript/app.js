4fff98ab6c7907b06a9252db229768ec52493aa9
SHIVAM KASHYAP <lootforecast@gmail.com>
2024-09-27 16:54:42 +0530

    array type defining on creation

--- typescript/app.js
+++ typescript/app.js
@@ -1,6 +1,14 @@
-var Btn = document.getElementById("btn");
-var add = function (n1, n2) {
+"use strict";
+// var ipt = document.getElementById("age-input") as HTMLInputElement;
+// ipt.value
+// ipt.checked
+// ipt.cloneNode
+// these all above values we will get as sugestions from ts bcz
+// we are explisitely spacefying the type of the element ie: HTMLInputElement while importing
+let ar = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var add = (n1, n2) => {
+    console.log("Array is: ", ar);
     return n1 + n2;
 };
-console.log("sum is: ".concat(add(1634, 10)));
+console.log(`sum is: ${add(1634, 10)}`);
 // console.log(`sum is: ${add("1", "10")}`);
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,16 +1,12 @@
-var ipt = document.getElementById("age-input") as HTMLInputElement;
+// var ipt = document.getElementById("age-input") as HTMLInputElement;
 // ipt.value
 // ipt.checked
 // ipt.cloneNode
-// these all above values we will get as sugestions from ts bcz 
-// we are explisitely spacefying the type of the element ie: HTMLInputElement while importing 
-
-
-
-
-
-
+// these all above values we will get as sugestions from ts bcz
+// we are explisitely spacefying the type of the element ie: HTMLInputElement while importing
+let ar: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var add = (n1: number, n2: number) => {
+  console.log("Array is: ", ar);
   return n1 + n2;
 };
 
